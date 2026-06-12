a8ce00c06bbd0375b9396f5c5a0493519c4352a2
Tysi3k22 <kacpert.kasiak@gmail.com>
2026-06-12 09:53:13 +0200

    poprawienie grawitacji i dodanie mechaniki skoku gracza

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-function Bird(x, y, dy, radius) {
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+function Bird(x, y, velocity, jumpForce, gravity, radius) {
     this.x = x;
     this.y = y;
-    this.dy = dy;
+    this.velocity = velocity;
+    this.jumpForce = jumpForce;
+    this.gravity = gravity;
     this.radius = radius;
 
     this.drawBird = () => {
@@ -12,12 +17,25 @@ function Bird(x, y, dy, radius) {
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctx.fillStyle = 'orange';
         ctx.fill();
+        ctx.closePath();
     }
 
-    this.updateBird = () => {
-        this.y += this.dy;
-
+    this.updateBird = () => { 
+        this.velocity += this.gravity;
+        this.y += this.velocity;
+        
+        this.jump();
         this.drawBird();
+    }
+
+    this.jump = () => {
+        document.addEventListener('keydown', (e) => {
+            if(e.key.includes(' ')) {
+                this.velocity = this.jumpForce;
+                e.preventDefault();
+            }
+
+        })
     }
 }
 
@@ -27,7 +45,7 @@ function main() {
     ctx.fillStyle = "gray";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    bird = new Bird(100, 75, 0.2, 20);
+    bird = new Bird(canvas.width / 2, canvas.height / 2, 0, -8, 0.5, 20);
 }
 
 function loop() {
